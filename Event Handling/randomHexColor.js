905baf450c1f0596a20fab6901d2927a84b0a5aa
guhan0000 <guhanm0000@gmail.com>
2026-07-10 18:04:47 +0530

    event handling added

---
+++ Event Handling/randomHexColor.js
@@ -0,0 +1,5 @@
+let div1 = document.getElementById("div1");
+let btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", () => {
+  div1.setAttribute("style", "background-color:tomato");
+});
